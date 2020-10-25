0b9764e6532159e2d0ff00e931183d4c9d886f73
racrow <nice6259@naver.com>
2020-10-25 17:56:21 +0900

    Updates

---
+++ precache-manifest.4a1ecfabb81a606c59b8f6fc533bd3f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf9e9c6af82815cbbeeff7f4714e44e",
+    "url": "/movie_app_201021/index.html"
+  },
+  {
+    "revision": "0d753235c17ac818d8f3",
+    "url": "/movie_app_201021/static/css/main.ddb28619.chunk.css"
+  },
+  {
+    "revision": "08347dcf369a3a02b3b3",
+    "url": "/movie_app_201021/static/js/2.c51a4964.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_app_201021/static/js/2.c51a4964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d753235c17ac818d8f3",
+    "url": "/movie_app_201021/static/js/main.1f749b48.chunk.js"
+  },
+  {
+    "revision": "5ff99a3b2ae4515e7cab",
+    "url": "/movie_app_201021/static/js/runtime-main.90fee630.js"
+  }
+]);
